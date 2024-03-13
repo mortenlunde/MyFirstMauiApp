18debe729256f79ea9e19397a20fdd41084da7c4
Morten Lunde <morten.lunde@live.no>
2024-03-13 17:19:51 +0100

    init project

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage
+{
+    private int _count;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        _count++;
+
+        CounterBtn.Text = _count == 1 ? $"Klikket {_count} gang" : $"Klikket {_count} ganger";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
